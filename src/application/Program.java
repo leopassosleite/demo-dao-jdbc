c4bf9e827ad5dc4d9597374afce56368a8230922
leopassosleite <leopassos.leite4@gmail.com>
2021-11-26 19:42:44 -0300

    Product entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Fogão");
+		Department obj = new Department(1, "Eletrônicos");
 		
-		System.out.println(obj);
+		Product product = new Product(21, "Xbox", new Date(), 6100.0, obj);
+		
+		System.out.println(product);
 
 	}
 
